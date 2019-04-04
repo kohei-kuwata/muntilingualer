cad47f2673215c598192c3040fd89773804b37ee
kuwata_kohei <kohei.multilingual@gmail.com>
2019-04-05 00:44:28 +0900

    Add select practice

--- app/src/main/java/com/example/muntilingualer/MainActivity.kt
+++ app/src/main/java/com/example/muntilingualer/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.muntilingualer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.btn_start)
+        val intent = Intent(this, SelectPracticeActivity::class.java)
+        btn.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
