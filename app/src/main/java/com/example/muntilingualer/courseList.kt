8a7f1729e4f15983fe104efb3af38b8afa73b66d
kuwata_kohei <kohei.multilingual@gmail.com>
2019-04-09 23:15:11 +0900

    rename file name

--- app/src/main/java/com/example/muntilingualer/courseList.kt
+++ app/src/main/java/com/example/muntilingualer/courseList.kt
@@ -19,7 +19,7 @@ class CourseList : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         container?.removeAllViews()
-        return inflater.inflate(R.layout.fragment_course_list, container, false)
+        return inflater.inflate(R.layout.fragment_practice, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
