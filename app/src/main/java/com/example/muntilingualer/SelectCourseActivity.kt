9f335702cf74632255034802e137611247639413
kuwata_kohei <kohei.multilingual@gmail.com>
2019-04-09 21:39:40 +0900

    fragment setting

--- app/src/main/java/com/example/muntilingualer/SelectCourseActivity.kt
+++ app/src/main/java/com/example/muntilingualer/SelectCourseActivity.kt
@@ -1,11 +1,57 @@
 package com.example.muntilingualer
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SelectCourseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.select_course)
+
+        val frgmManager = supportFragmentManager
+        val frgmTransaction = frgmManager.beginTransaction()
+
+        val frgm01 = CourseList.newInstance("List1")
+        val frgm02 = CourseList.newInstance("List2")
+        val frgm03 = CourseList.newInstance("List3")
+
+        val frgmArray = arrayOf(frgm01, frgm02, frgm03)
+        val arraySize = frgmArray.size -1
+        var currentFrgm = frgm01
+        var position = 0
+
+        frgmTransaction.add(R.id.course_list, currentFrgm)
+        frgmTransaction.commit()
+
+        val btn_back = findViewById<Button>(R.id.btn_list_back)
+        btn_back.setOnClickListener {
+            val frgmTransaction = supportFragmentManager.beginTransaction()
+            position = frgmArray.indexOf(currentFrgm)
+            if (position == 0) {
+            } else {
+                position -= 1
+            }
+            currentFrgm = frgmArray[position]
+
+            frgmTransaction.replace(R.id.course_list, currentFrgm)
+            frgmTransaction.commit()
+        }
+
+        val btn_next = findViewById<Button>(R.id.btn_list_next)
+        btn_next.setOnClickListener {
+            val frgmTransaction = supportFragmentManager.beginTransaction()
+            position = frgmArray.indexOf(currentFrgm)
+            if (position == arraySize) {
+            } else {
+                position += 1
+            }
+            currentFrgm = frgmArray[position]
+
+
+            frgmTransaction.replace(R.id.course_list, currentFrgm)
+            frgmTransaction.commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/muntilingualer/courseList.kt
@@ -0,0 +1,52 @@
+package com.example.muntilingualer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class CourseList : Fragment() {
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        container?.removeAllViews()
+        return inflater.inflate(R.layout.fragment_course_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments
+        val viewTxt = if (args == null) {
+            ""
+        } else {
+            args.getString(TXT)
+        }
+
+        val listTextView = view.findViewById<TextView>(R.id.frgm_list_txt)
+        listTextView.text = viewTxt
+    }
+
+    companion object {
+        private const val TXT = "txt"
+
+        fun newInstance(txt: String): CourseList {
+            val courseList = CourseList()
+            val args = Bundle()
+            args.putString(TXT, txt)
+            courseList.arguments = args
+            return courseList
+
+        }
+
+    }
+}
