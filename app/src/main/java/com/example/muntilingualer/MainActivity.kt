f1c4373db8eece1bfd5b0f43ddc854c68432a764
kuwata_kohei <kohei.multilingual@gmail.com>
2019-04-18 03:27:33 +0900

    add Select Language Activity

--- app/src/main/java/com/example/muntilingualer/MainActivity.kt
+++ app/src/main/java/com/example/muntilingualer/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.muntilingualer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private val CODE_REQUEST = 1
+    private val CODE_FROM = 0
+    private val CODE_TO = 1
+    private lateinit var BTN_LANG_FROM: Button
+    private lateinit var BTN_LANG_TO: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +21,30 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SelectPracticeTypeActivity::class.java)
         btn.setOnClickListener {
             startActivity(intent)
+        }
+
+        val intentLang = Intent(this, SelectLanguageActivity::class.java)
+
+        BTN_LANG_FROM = findViewById(R.id.btn_lang_from)
+        BTN_LANG_FROM.setOnClickListener {
+            intentLang.putExtra("btn_code", CODE_FROM)
+            startActivityForResult(intentLang, CODE_REQUEST)
+        }
+
+        BTN_LANG_TO = findViewById(R.id.btn_lang_to)
+        BTN_LANG_TO.setOnClickListener {
+            intentLang.putExtra("btn_code", CODE_TO)
+            startActivityForResult(intentLang, CODE_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CODE_REQUEST) {
+            if (resultCode == CODE_FROM) {
+                BTN_LANG_FROM.text = data?.getStringExtra("LANG") ?: "JPN"
+            }else if (resultCode == CODE_TO) {
+                BTN_LANG_TO.text = data?.getStringExtra("LANG") ?: "ENG"
+            }
         }
     }
 }
--- app/src/main/java/com/example/muntilingualer/SelectLanguageActivity.kt
+++ app/src/main/java/com/example/muntilingualer/SelectLanguageActivity.kt
@@ -1,42 +1,37 @@
 package com.example.muntilingualer
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Adapter
+import android.util.Log
 import android.widget.AdapterView
 import android.widget.ListView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.marginTop
 
-class SelectCourseActivity : AppCompatActivity() {
+class SelectLanguageActivity : AppCompatActivity() {
     private val courseItem = HashMap<String, ArrayList<SelectCourseItem>>()
 
-    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_course)
+        setContentView(R.layout.activity_select_language)
 
         loadCourse()
 
-        val lv = findViewById<ListView>(R.id.lView_course_list)
+        val lv = findViewById<ListView>(R.id.lView_select_language)
         val courses = getCourse("set")
         val courseAdapter = SelectCourseAdapter(courses, this)
         lv.adapter = courseAdapter
 
         lv.onItemClickListener = AdapterView.OnItemClickListener(function = { _, _, i, _ ->
-            //Toast.makeText(this@SelectCourseActivity, "you selected " + (i + 1) + ":" + courses[i].title, Toast.LENGTH_LONG).show()
 
-            val intent = Intent(this, PracticeActivity::class.java)
-            intent.putExtra("CourseTitle", courses[i].title)
-            startActivity(intent)
+            intent.putExtra("LANG", courses[i].title)
+            setResult(intent.getIntExtra("btn_code", 0 ), intent)
+            finish()
         })
     }
 
-    private fun getCourse(couseName: String): ArrayList<SelectCourseItem>{
-        val titleName = courseItem[couseName]
-        if (titleName != null) return titleName else return ArrayList<SelectCourseItem>()
+    private fun getCourse(courseName: String): ArrayList<SelectCourseItem>{
+        val titleName = courseItem[courseName]
+        return titleName ?: ArrayList()
     }
 
     private fun loadCourse() {
@@ -48,19 +43,20 @@ class SelectCourseActivity : AppCompatActivity() {
 
         var course = SelectCourseItem()
         course.number = 1
-        course.title = "title001"
+        course.title = "JPN"
         courseList.add(course)
 
         course = SelectCourseItem()
         course.number = 2
-        course.title = "title002"
+        course.title = "ENG"
         courseList.add(course)
 
         course = SelectCourseItem()
         course.number = 3
-        course.title = "title003"
+        course.title = "CNI"
         courseList.add(course)
 
         courseItem["set"] = courseList
     }
+
 }
